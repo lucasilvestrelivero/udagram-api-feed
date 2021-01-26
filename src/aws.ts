29962a3c769da8a1834fb38d45e67283423d2342
Lucas Silvestre Livero-LSIL <lsil@motorola.com>
2021-01-26 11:28:17 -0400

    Error s3 url

--- src/aws.ts
+++ src/aws.ts
@@ -1,9 +1,11 @@
 import AWS = require('aws-sdk');
 import {config} from './config/config';
 
-//Configure AWS
-var credentials = new AWS.SharedIniFileCredentials({profile: 'default'});
-AWS.config.credentials = credentials;
+if (process.env.AWS_PROFILE !== "DEPLOYED") {
+  //Configure AWS
+  var credentials = new AWS.SharedIniFileCredentials({profile: 'default'});
+  AWS.config.credentials = credentials;
+}
 
 export const s3 = new AWS.S3({
   signatureVersion: 'v4',
